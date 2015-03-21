fd38dfd9d698696db880f851b59735278ed44501
Koji Ishii <kojii@chromium.com>
2015-03-22 00:21:28 +0900

    Change bookmarklet link to a tag

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ gulp.task('bookmarklet', function () {
 
 gulp.task('README', ['bookmarklet'], function () {
     return gulp.src('README.md')
-        .pipe(replace(/^\[bookmarklet]: .*$/m, '[bookmarklet]: ' + encodeURI(fs.readFileSync(bookmarklet))))
+        .pipe(replace(/(<a id="bookmarklet" href=")[^"]*/, '$1' + fs.readFileSync(bookmarklet)))
         .pipe(gulp.dest('.'));
 });
 
