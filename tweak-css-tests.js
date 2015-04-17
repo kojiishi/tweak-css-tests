2eb48d5e478a13e679584f78e37cce23fdff8089
Koji Ishii <kojiishi@gmail.com>
2015-04-17 17:03:29 +0900

    Minor refactoring

--- tweak-css-tests.js
+++ tweak-css-tests.js
@@ -12,10 +12,7 @@
         var tweaker = new Tweaker();
         var isModified = false;
         forEach(doc.querySelectorAll("style"), function (style) {
-            var original = style.innerText;
-            tweaker.text = original;
-            tweaker.tweak();
-            if (tweaker.text == original)
+            if (!tweaker.tweakStyleText(style.innerText))
                 return;
             style.innerHTML = tweaker.text;
             isModified = true;
@@ -35,10 +32,16 @@
             recalc(doc.body);
     }
 
+    proto.tweakStyleText = function (text) {
+        this.text = text;
+        this.tweakPlatform();
+        return this.text != text;
+    };
+
     var style = document.body.style;
     if ('webkitWritingMode' in style) {
         proto.prefix = "-webkit-";
-        proto.tweak = function tweakWebKit() {
+        proto.tweakPlatform = function tweakWebKit() {
             this.rename("text-combine-(horizontal|upright)", "all", "-webkit-text-combine", "horizontal");
             this.renameProperty("text-combine-(horizontal|upright)", "-webkit-text-combine");
             this.renameValue("text-orientation", "mixed", "vertical-right");
@@ -47,7 +50,7 @@
             this.replace(/(-webkit-){2,}/g, "-webkit-");
         };
     } else if ('msTextCombineHorizontal' in style) {
-        proto.tweak = function tweakIE() {
+        proto.tweakPlatform = function tweakIE() {
             this.renameProperty("text-combine-(horizontal|upright)", "-ms-text-combine-horizontal");
             this.renameValue("writing-mode",
                 "horizontal-tb", "lr-tb",
@@ -64,31 +67,31 @@
             property = arguments[i];
             this.renameProperty(property, this.prefix + property);
         }
-    }
+    };
 
     proto.prefixValue = function (property, value) {
         for (var i = 1; i < arguments.length; i++) {
             value = arguments[i];
             this.renameValue(property, value, this.prefix + value);
         }
-    }
+    };
 
     proto.renameProperty = function (property, to) {
         this.rename(property, "", to, "");
-    }
+    };
 
     proto.renameValue = function (property, value, to) {
         for (var i = 1; i < arguments.length; i += 2)
             this.rename(property, arguments[i], property, arguments[i+1]);
-    }
+    };
 
     proto.rename = function (property, value, propertyTo, valueTo) {
         this.replace(new RegExp(property + "\\s*:\\s*" + value, "g"), propertyTo + ": " + valueTo);
-    }
+    };
 
     proto.replace = function (from, to) {
         this.text = this.text.replace(from, to);
-    }
+    };
 
     function recalc(element) {
         var saved = element.style.display;
